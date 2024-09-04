8081425f83140beb3faf062349c7ee0438519ba9
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-09-04 19:36:08 +0200

    fix issues with baseUrl

--- src/redux/services/api.js
+++ src/redux/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://task-pro-88l4.onrender.com';
+const BASE_URL = 'https://task-pro-88l4.onrender.com';
 
 //--------------------auth-------------------//
 export const setAuthHeader = token => {
@@ -10,36 +10,38 @@ const clearAuthHeader = () => {
   axios.defaults.headers.common.Authorization = '';
 };
 
+
 export async function registration(credentials) {
-  const response = await axios.post('api/users/register', credentials);
+  const response = await axios.post(`${BASE_URL}/api/users/register`, credentials);
   setAuthHeader(response.data.token);
   return response.data;
 }
 
+
 export async function logIn(credentials) {
-  const response = await axios.post('api/users/login', credentials);
+  const response = await axios.post(`${BASE_URL}/api/users/login`, credentials);
   setAuthHeader(response.data.user.token);
   return response.data;
 }
 
 export async function logOut() {
-  await axios.post('api/users/logout');
+  await axios.post(`${BASE_URL}/api/users/logout`);
   clearAuthHeader();
 }
 
 export async function refresh() {
-  const response = await axios.get('api/users/current');
+  const response = await axios.get(`${BASE_URL}/api/users/current`);
   return response.data;
 }
 
 //--------------------user-------------------//
 export async function support(credentials) {
-  const response = await axios.post('api/help', credentials);
+  const response = await axios.post(`${BASE_URL}/api/help`, credentials);
   return response.data;
 }
 
 export async function editUser(data) {
-  const response = await axios.patch('api/users/update', data, {
+  const response = await axios.patch(`${BASE_URL}/api/users/update`, data, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
@@ -49,58 +51,58 @@ export async function editUser(data) {
 
 //--------------------board-------------------//
 export async function addBoard(data) {
-  const response = await axios.post('api/boards', data);
+  const response = await axios.post(`${BASE_URL}/api/boards`, data);
   return response.data;
 }
 
 export async function getBoardById(boardId) {
-  const response = await axios.get(`api/boards/${boardId}`);
+  const response = await axios.get(`${BASE_URL}/api/boards/${boardId}`);
   return response.data;
 }
 
 export async function deleteBoard(boardId) {
-  const response = await axios.delete(`api/boards/${boardId}`);
+  const response = await axios.delete(`${BASE_URL}/api/boards/${boardId}`);
   return response.data;
 }
 
 export async function editBoard(boardId, data) {
-  const response = await axios.patch(`api/boards/${boardId}`, data);
+  const response = await axios.patch(`${BASE_URL}/api/boards/${boardId}`, data);
   return response.data;
 }
 
 //--------------------columns-------------------//
 export async function addColumn(data) {
-  const response = await axios.post('api/columns', data);
+  const response = await axios.post(`${BASE_URL}/api/columns`, data);
   return response.data;
 }
 
 export async function editColumn(columnId, data) {
-  const response = await axios.patch(`api/columns/${columnId}`, data);
+  const response = await axios.patch(`${BASE_URL}/api/columns/${columnId}`, data);
   return response.data;
 }
 
 export async function deleteColumnById(columnId) {
-  const response = await axios.delete(`api/columns/${columnId}`);
+  const response = await axios.delete(`${BASE_URL}/api/columns/${columnId}`);
   return response.data;
 }
 
 //--------------------tasks-------------------//
 export async function addTask(data) {
-  const response = await axios.post('api/tasks', data);
+  const response = await axios.post(`${BASE_URL}/api/tasks`, data);
   return response.data;
 }
 
 export async function editTaskById(taskId, data) {
-  const response = await axios.patch(`api/tasks/${taskId}`, data);
+  const response = await axios.patch(`${BASE_URL}/api/tasks/${taskId}`, data);
   return response.data;
 }
 
 export async function deleteTaskById(taskId) {
-  const response = await axios.delete(`api/tasks/${taskId}`);
+  const response = await axios.delete(`${BASE_URL}/api/tasks/${taskId}`);
   return response.data;
 }
 
 export async function replaceTask(taskId, column) {
-  const response = await axios.post(`api/tasks/${taskId}/replace`, { column });
+  const response = await axios.post(`${BASE_URL}/api/tasks/${taskId}/replace`, { column });
   return response.data;
 }
